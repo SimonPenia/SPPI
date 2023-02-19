b6ad3a74e15f1fcd2c8c746a9d1695eac321901d
SimonPenia <104177456+SimonPenia@users.noreply.github.com>
2023-02-19 00:00:58 -0300

    Major changes
    Changed command names.
    Changed how it detects commands.
    Switched (no pun intended) from if statements to case statements to select an action.
    
    Should be faster than it already was.

--- SPPI.cs
+++ SPPI.cs
@@ -9,73 +9,91 @@ namespace SPPI
 		{
 			int[] prices = {70, 60, 55, 150, 80, 20, 20, 25, 80, 10, 20, 40, 40, 40, 40, 30, 60, 10, 15};
 			int[] totalPrices = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+			int selectedCmd = 0;
 			bool open = true;
-			string cmdInput, purCmd = "purchase";
+			string cmdInput;
 			string[] products =
 			{"apples", "bananas", "oranges", "milk",
 			 "flour", "beds", "baths", "beyond",
 			 "redbull", "ice", "cake", "laptops",
 			 "pepsi", "pizza", "bacon", "condoms",
 			 "cookies", "pollos", "methylamine"};
-			string[] adminCmds = {"cmdlist", "prdlist", "money", "exit"};
+			string[] adminCmds = {"help", "products", "buy", "money", "exit"};
 			
 			while(open == true)
 			{
 				Console.Write("-> ");
 				cmdInput = Console.ReadLine();
-				for(int i = 0; i < 19; i++)
+				
+				for(int i = 0; i < adminCmds.Length; i++)
 				{
-					if(cmdInput == purCmd + " " + products[i])
+					if(!cmdInput.StartsWith(adminCmds[i]))
 					{
-						totalPrices[i] += prices[i];
-						Console.WriteLine("The product \"" + products[i] + "\" has been purchased.");
+						selectedCmd = -1;
+					}
+					if(cmdInput.StartsWith(adminCmds[i]))
+					{
+						selectedCmd = i;
+						Console.WriteLine(selectedCmd + "");
+						break;
 					}
 				}
 				
-				if(cmdInput == adminCmds[0])
+				switch(selectedCmd)
 				{
-					Console.WriteLine("Command list:");
-					Console.Write(purCmd + ", ");
-					for(int i = 0; i < 3; i++)
-					{
-						Console.Write(adminCmds[i] + ", ");
-					}
-					Console.WriteLine(adminCmds[3] + ".");
-				}
-				if(cmdInput == adminCmds[1])
-				{
-					Console.WriteLine("Products:");
-					for(int i = 0; i < 18; i++)
-					{
-						Console.Write(products[i] + ", ");
-					}
-					Console.WriteLine(products[18] + ".");
-				}
-				if(cmdInput == adminCmds[2])
-				{
-					Console.WriteLine("Money earned so far:");
-					for(int i = 0; i < 18; i++)
-					{
-						Console.Write(products[i] + ": ");
-						Console.Write(totalPrices[i] + ", ");
-					}
-					Console.Write(products[18] + ": ");
-					Console.WriteLine(totalPrices[18] + ".");
-				}
-				if(cmdInput == adminCmds[3])
-				{
-					using(StreamWriter sw = new StreamWriter("MONEY.TXT"))
-					{
-						sw.WriteLine("Money earned so far:");
+					case 0:
+						Console.WriteLine("Command list:");
+						for(int i = 0; i < adminCmds.Length - 1; i++)
+						{
+							Console.Write(adminCmds[i] + ", ");
+						}
+						Console.WriteLine(adminCmds[adminCmds.Length - 1] + ".");
+						break;
+					case 1:
+						Console.WriteLine("Products:");
+						for(int i = 0; i < products.Length - 1; i++)
+						{
+							Console.Write(products[i] + ", ");
+						}
+						Console.WriteLine(products[products.Length - 1] + ".");
+						break;
+					case 2:
+						for(int i = 0; i < products.Length; i++)
+						{
+							if(cmdInput.EndsWith(products[i]))
+							{
+								totalPrices[i] += prices[i];
+								Console.WriteLine("The product \"" + products[i] + "\" has been purchased for the price of $" + prices[i] + ".");
+							}
+						}
+						break;
+					case 3:
+						Console.WriteLine("Money earned so far:");
 						for(int i = 0; i < 18; i++)
 						{
-							sw.Write(products[i] + ": ");
-							sw.Write(totalPrices[i] + ", ");
+							Console.Write(products[i] + ": ");
+							Console.Write(totalPrices[i] + ", ");
 						}
-						sw.Write(products[18] + ": ");
-						sw.WriteLine(totalPrices[18] + ".");
-					}
-					open = false;
+						Console.Write(products[18] + ": ");
+						Console.WriteLine(totalPrices[18] + ".");
+						break;
+					case 4:
+						using(StreamWriter sw = new StreamWriter("MONEY.TXT"))
+						{
+							sw.WriteLine("Money earned so far:");
+							for(int i = 0; i < products.Length; i++)
+							{
+								sw.Write(products[i] + ": ");
+								sw.Write(totalPrices[i] + ", ");
+							}
+							sw.Write(products[products.Length - 1] + ": ");
+							sw.WriteLine(totalPrices[totalPrices.Length - 1] + ".");
+						}
+						open = false;
+						break;
+					default:
+						Console.WriteLine("Command \"" + cmdInput + "\" was not found.");
+						break;
 				}
 			}
 		}
