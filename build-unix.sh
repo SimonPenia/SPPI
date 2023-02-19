54a58997df7833ca9bb30ee09e8d7728c315e214
SimonPenia <104177456+SimonPenia@users.noreply.github.com>
2023-02-19 04:41:29 -0300

    Update build-unix.sh

--- build-unix.sh
+++ build-unix.sh
@@ -2,5 +2,6 @@
 
 # Probably should add parameters so the user can choose between the two,
 # however I have no idea how to do that and I'm too lazy to learn.
+
 mcs SPPI.cs -out:SPPI
 mcs SPPI.cs -out:SPPI.exe
